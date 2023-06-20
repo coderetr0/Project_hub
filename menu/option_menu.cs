e8b04cc9e0e66c9bb77fff6af8cf92a3ea035f85
dan-ali.mabanza <dan-ali.mabanza@epitech.eu>
2023-06-20 17:08:31 +0200

    menu, demarrer une partie, quitter la partie et revenir au menu principal

---
+++ menu/option_menu.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class option_du_menu : MonoBehaviour
+{
+    public void Retour_au_menu_principal()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
